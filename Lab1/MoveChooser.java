6a384b57b96d517c6397afe56b828c3d0af71e3e
Rui Xu <rui.xu-11@student.manchester.ac.uk>
2022-10-09 23:41:15 +0100

    First-time finished

--- Lab1/MoveChooser.java
+++ Lab1/MoveChooser.java
@@ -2,14 +2,109 @@ import java.util.ArrayList;
 
 public class MoveChooser {
   
+    // numbers reflect the value for a player of being on the respective square
+    public static int positionValue[][] = {   
+        {120, -20, 20, 5, 5, 20, -20, 120},
+        {-20, -40, -5, -5, -5, -5, -40, -20},
+        {20, -5, 15, 3, 3, 15, -5, 20},
+        {5, -5, 3, 3, 3, 3, -5, 5},
+        {5, -5, 3, 3, 3, 3, -5, 5},
+        {20, -5, 15, 3, 3, 15, -5, 20},
+        {-20, -40, -5, -5, -5, -5, -40, -20},
+        {120, -20, 20, 5, 5, 20, -20, 120},
+    };
+
     public static Move chooseMove(BoardState boardState){
 
-	int searchDepth= Othello.searchDepth;
 
         ArrayList<Move> moves= boardState.getLegalMoves();
         if(moves.isEmpty()){
             return null;
 	}
-        return moves.get(0);
+        // return moves.get(0);
+        return topLevel(boardState, moves);
     }
+
+
+    ///////////////////////////////////////////////// start
+    // board position = + weights of all those squares occupied by white pieces - weights of those squares occupied by black pieces.
+    public static int staticEvaluation(BoardState boardState){
+    	int value = 0;
+    	for(int i=0; i<8; i++){
+    		for(int j=0; j<8; j++){
+                // white
+    			if(boardState.getContents(i, j)== 1){
+    				value += positionValue[i][j];
+                // black
+    			}else if(boardState.getContents(i,j)== -1){
+    				value -= positionValue[i][j];
+    			}
+    		}
+    	}
+    		return value;
+    } 
+
+    // αβ-pruning DFS
+    public static int miniMax(BoardState boardState, int searchDepth, int alpha, int beta, Boolean maxmize){
+    	ArrayList<Move> move = boardState.getLegalMoves();
+
+    	if(searchDepth==0){
+    		return staticEvaluation(boardState);
+    	}else if(maxmize){
+    		alpha = Integer.MIN_VALUE;  // lower bound
+
+    		for(int i =0 ; i<move.size(); i++){
+    			if(alpha >= beta||move.isEmpty()){
+    				break;
+    			}
+                // a fresh copy of boardState
+    			BoardState temp = boardState.deepCopy();
+    			temp.makeLegalMove(move.get(i).x, move.get(i).y);
+    			int miniVal = miniMax(temp, searchDepth-1, alpha, beta, false);
+    			if(alpha < miniVal){
+    				alpha = miniVal;
+    			}
+    		}
+    		return alpha;
+    	}else{
+    		beta = Integer.MAX_VALUE;   // upper bound
+
+    		for(int i = 0; i<move.size(); i++){
+    			if(alpha >= beta||move.isEmpty()){
+    				break;
+    			}
+                // a fresh copy of boardState
+    			BoardState temp = boardState.deepCopy();
+    			temp.makeLegalMove(move.get(i).x, move.get(i).y);
+    			int maxVal = miniMax(temp, searchDepth-1, alpha, beta, true);
+    			if(maxVal < beta){
+    				beta = maxVal;
+    			}
+    		}
+    		return beta;
+    	}
+    }
+
+    // The top-level call
+    public static Move topLevel(BoardState bs, ArrayList<Move> move){
+        int searchDepth= Othello.searchDepth;
+    	int valueIndex = 0;
+    	int a = Integer.MIN_VALUE;
+    	int b = Integer.MAX_VALUE;
+    	Move bestmove = null;
+
+    	for(int i=0; i<move.size(); i++){
+    		BoardState temp = bs.deepCopy();
+    		temp.makeLegalMove(move.get(i).x, move.get(i).y);
+    		int minimax = miniMax(temp, searchDepth-1, a, b, false);
+    		if(a<minimax){
+    			a=minimax;
+    			valueIndex = i;
+    		}
+    	}
+
+    	bestmove = move.get(valueIndex);
+    	return bestmove;
+    }
+ 
 }
