d4fd4f7bdcd40e7ee7699f43018a9190a5b4e0da
Ray7788 <ray778@foxmail.com>
2022-10-14 12:27:33 +0100

    Lab1 Reversi Summative Corrected

--- Lab1/MoveChooser.java
+++ Lab1/MoveChooser.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;  
 
 public class MoveChooser {
-  
+	// this class is designed for computer player
     // numbers reflect the value for a player of being on the respective square
     public static int positionValue[][] = {   
         {120, -20, 20, 5, 5, 20, -20, 120},
@@ -20,13 +20,14 @@ public class MoveChooser {
         ArrayList<Move> moves= boardState.getLegalMoves();
         if(moves.isEmpty()){
             return null;
-	}
+		}
         // return moves.get(0);
         return topLevel(boardState, moves);
     }
 
 
-    ///////////////////////////////////////////////// start
+    /////////////////////////////////////////////////  write a better move selection function ///////////////////////////////////////
+
     // board position = + weights of all those squares occupied by white pieces - weights of those squares occupied by black pieces.
     public static int staticEvaluation(BoardState boardState){
     	int value = 0;
@@ -34,7 +35,7 @@ public class MoveChooser {
     		for(int j=0; j<8; j++){
                 // white
     			if(boardState.getContents(i, j)== 1){
-    				value += positionValue[i][j];
+    				value += positionValue[i][j]; 
                 // black
     			}else if(boardState.getContents(i,j)== -1){
     				value -= positionValue[i][j];
@@ -47,22 +48,22 @@ public class MoveChooser {
     // αβ-pruning DFS
     public static int miniMax(BoardState boardState, int searchDepth, int alpha, int beta, Boolean maxmize){
     	ArrayList<Move> move = boardState.getLegalMoves();
-
+		//
     	if(searchDepth==0){
     		return staticEvaluation(boardState);
     	}else if(maxmize){
     		alpha = Integer.MIN_VALUE;  // lower bound
 
-    		for(int i =0 ; i<move.size(); i++){
+    		for(int i = 0 ; i<move.size(); i++){
     			if(alpha >= beta||move.isEmpty()){
     				break;
     			}
                 // a fresh copy of boardState
-    			BoardState temp = boardState.deepCopy();
-    			temp.makeLegalMove(move.get(i).x, move.get(i).y);
-    			int miniVal = miniMax(temp, searchDepth-1, alpha, beta, false);
-    			if(alpha < miniVal){
-    				alpha = miniVal;
+    			BoardState boardState1 = boardState.deepCopy();
+    			boardState1.makeLegalMove(move.get(i).x, move.get(i).y);
+    			int miniVal = miniMax(boardState1, searchDepth-1, alpha, beta, false);	// next is minimizing
+    			if(alpha < miniVal){	// update alpha
+    				alpha = miniVal;	
     			}
     		}
     		return alpha;
@@ -74,10 +75,10 @@ public class MoveChooser {
     				break;
     			}
                 // a fresh copy of boardState
-    			BoardState temp = boardState.deepCopy();
-    			temp.makeLegalMove(move.get(i).x, move.get(i).y);
-    			int maxVal = miniMax(temp, searchDepth-1, alpha, beta, true);
-    			if(maxVal < beta){
+    			BoardState boardState1 = boardState.deepCopy();
+    			boardState1.makeLegalMove(move.get(i).x, move.get(i).y);
+    			int maxVal = miniMax(boardState1, searchDepth-1, alpha, beta, true);	// next is maxmizing
+    			if(maxVal < beta){		// update beta
     				beta = maxVal;
     			}
     		}
@@ -89,14 +90,14 @@ public class MoveChooser {
     public static Move topLevel(BoardState bs, ArrayList<Move> move){
         int searchDepth= Othello.searchDepth;
     	int valueIndex = 0;
-    	int a = Integer.MIN_VALUE;
-    	int b = Integer.MAX_VALUE;
-    	Move bestmove = null;
+    	int a = Integer.MIN_VALUE;	// negative infinity
+    	int b = Integer.MAX_VALUE;	// positive infinity
+    	Move bestmove = null;	// set when there are no moves available as the default value 
 
     	for(int i=0; i<move.size(); i++){
     		BoardState temp = bs.deepCopy();
     		temp.makeLegalMove(move.get(i).x, move.get(i).y);
-    		int minimax = miniMax(temp, searchDepth-1, a, b, false);
+    		int minimax = miniMax(temp, searchDepth-1, a, b, false);	// at the beginning set to minimizing node
     		if(a<minimax){
     			a=minimax;
     			valueIndex = i;
