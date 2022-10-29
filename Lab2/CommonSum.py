4220ffa175da14bb704148857faa5a2f3d844a7c
Ray7788 <ray778@foxmail.com>
2022-10-29 20:51:01 +0100

    Task3 finished

--- Lab2/CommonSum.py
+++ Lab2/CommonSum.py
@@ -1,17 +1,23 @@
 from constraint import Problem , AllDifferentConstraint, ExactSumConstraint
 
 def CommonSum(n):
+    return int((1 + n * n) * n / 2)
+
+def msqList(n, pairList):
     problem = Problem()
     problem.addVariables (range(0 , n * n ) , range(1 , n * n +1))
     problem.addConstraint(AllDifferentConstraint() , range(0 , n * n ))
 
+    for v, i in pairList:
+        problem.addConstraint(ExactSumConstraint(i), [v])
+    
     for row in range ( n ):
         problem.addConstraint(ExactSumConstraint(CommonSum(n)) ,
         [ row * n + i for i in range ( n )])
 
     for col in range ( n ):
         problem.addConstraint(ExactSumConstraint(CommonSum(n)) ,
-        [ col * n + i for i in range ( n )])
+        [ col + n * i for i in range ( n )])
 
     # restrict diagonal top-left to bot-right
     problem.addConstraint(ExactSumConstraint(CommonSum(n)), [i for i in range(0, n**2, n+1)])
@@ -21,6 +27,5 @@ def CommonSum(n):
 
     solns = problem.getSolutions()
     return print (solns)
-    # print (solns)
 
-CommonSum(1)
+msqList(4,[[0,13],[1,12],[2,7]])
