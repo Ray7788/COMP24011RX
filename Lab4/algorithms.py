7a568fcbf3fe09462c63e326890b168714b22744
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2022-12-08 12:39:01 +0000

    Complete task1

--- Lab4/algorithms.py
+++ Lab4/algorithms.py
@@ -5,8 +5,18 @@ import cv2
 
 def dist_thresholding(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1, des2, k = len(des1))
+    matchSet = []
 
+    for i in range(0,len(matches)):
+        matches_curr = []
+        for match in matches[i]:
+            if match.distance < threshold_value:
+                matches_curr.append(match)
+            else:
+                break
+        matchSet.append(matches_curr)
+    return matchSet
 
 def nn(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
